61c682c7c39a8440b7a61f8b7d84343d9842f502
Robbie Capps <rocapp@gmail.com>
2026-03-25 00:12:34 -0700

    updated flake, confirmed build on nixos

--- flake.nix
+++ flake.nix
@@ -3,13 +3,16 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     naersk.url = "github:nix-community/naersk";
+    devenv.url = "github:cachix/devenv";
   };
 
   outputs = {
     self,
     flake-utils,
     naersk,
-    nixpkgs,
+      nixpkgs,
+      devenv,
+      ...
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = (import nixpkgs) {inherit system;};
